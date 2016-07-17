f58c90899c9c45fd7693895323e504bbf7ac64fb
Vivek Anand <vivekanand1101@gmail.com>
2016-07-17 21:07:58 +0530

    Openshift attempt #1

---
+++ wsgi/xyz/wsgi.py
@@ -0,0 +1,29 @@
+"""
+WSGI config for xyz project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+
+import os
+import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+     sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'xyz'))
+     virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+     os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+     virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+     try:
+         execfile(virtualenv, dict(__file__=virtualenv))
+     except IOError:
+         pass
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
